551744cd28222eb5a2df708a533902f0dfdd72ba
Stephen Lead <stephen.lead@gmail.com>
2015-05-03 20:18:25 +1000

    Add "Agency" CSS file. Start incorporating it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426111929) do
+ActiveRecord::Schema.define(version: 20150426111733) do
 
   create_table "cafes", force: true do |t|
     t.string   "name"
@@ -21,15 +21,6 @@ ActiveRecord::Schema.define(version: 20150426111929) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-  end
-
-  create_table "caves", force: true do |t|
-    t.string   "name"
-    t.string   "address"
-    t.string   "city"
-    t.string   "country"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
