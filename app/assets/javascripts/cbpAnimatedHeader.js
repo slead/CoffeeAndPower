aa94f783ffbec13c8db43ba83ea9e1842807085b
Stephen Lead <stephen.lead@gmail.com>
2015-05-04 21:42:12 +1000

    Hook up the animated header
    Required hacking into the cbpAnimatedHeader.js file and hard-coding the
    class of the header.
    
    Still need to fix the User dropdown

--- app/assets/javascripts/cbpAnimatedHeader.js
+++ app/assets/javascripts/cbpAnimatedHeader.js
@@ -13,7 +13,7 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-default' ),
 		didScroll = false,
-		changeHeaderOn = 300;
+		changeHeaderOn = 100;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
@@ -27,10 +27,10 @@ var cbpAnimatedHeader = (function() {
 	function scrollPage() {
 		var sy = scrollY();
 		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
+			$('.navbar-default').addClass("navbar-shrink");
 		}
 		else {
-			classie.remove( header, 'navbar-shrink' );
+			$('.navbar-default').removeClass("navbar-shrink");
 		}
 		didScroll = false;
 	}
