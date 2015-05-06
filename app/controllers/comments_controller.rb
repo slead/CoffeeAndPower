785dc82da0af3ebea24f34795f4c4f071d53e470
Stephen Lead <stephen.lead@gmail.com>
2015-05-06 19:05:49 +1000

    Display the Comment author's name

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
     def create
       @cafe = Cafe.find(params[:cafe_id])
       @comment = @cafe.comments.create(params[:comment].permit(:comment))
+      @comment.user = current_user
 
       if @comment.save
         redirect_to cafe_path(@cafe)
