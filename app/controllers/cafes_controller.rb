2d826fa0a355b2debb17e54ad3ebb38719d1a820
Stephen Lead <stephen.lead@gmail.com>
2015-05-11 21:11:04 +1000

    Add style to the pagination
    including the will_paginate_bootstrap gem

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -10,14 +10,14 @@ class CafesController < ApplicationController
 	def index
     if params[:search].present?
       @cafes = Cafe.search(params[:search], 
-        page: params[:page], per_page: 6,
+        page: params[:page], per_page: 3,
         order: {name: :asc}
       )
       if @cafes.count == 0
         flash[:notice] = 'Sorry, no cafes were found.'
       end
     else
-      @cafes = Cafe.all.order("CREATED_AT").paginate(:page => params[:page], :per_page => 6)
+      @cafes = Cafe.all.order("CREATED_AT").paginate(:page => params[:page], :per_page => 3)
     end
 	end
 
